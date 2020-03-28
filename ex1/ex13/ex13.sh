138b15cb658d659f7b3dd3600e35701962efeff8
idoz <ido.ze2@gmail.com>
2020-03-28 21:06:43 +0300

    ex1, 3rd script

---
+++ ex1/ex13/ex13.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Assert arguments passed.
+if [ -z "$1" ]
+then
+	echo "Please provide a directory to list."
+	exit
+elif [ -z "$2" ]
+then
+	echo "Please provide a file to search."
+	exit
+fi
+#Iterate through all files and subdirs of level 1.
+for L1 in $(dir $1)
+do
+	#Test if file is a directory.
+	if [[ -d $1/$L1 ]]
+	then
+		#Iterate through all files and subdirs of level 2.
+		for L2 in $(dir $1/$L1)
+		do
+			#Test filename equality of L2 File.
+			if [ $L2 == $2 ]
+			then
+				echo $1/$L1/$L2
+			fi
+		done
+	#Test filename equality of L1 File.
+	elif [ $L1 == $2  ]
+	then
+		echo $1/$L1 
+	fi
+done
